b6e06a26c1b298560b1ed22d78c70171d1ea67b3
HZ-1 <601661706@qq.com>
2019-03-21 00:53:30 +0800

    Http.request 服务端 跨域请求的demo--->如何通过服务端转发请求，解决跨域问题

--- server/index.js
+++ server/index.js
@@ -1,32 +1,77 @@
-const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')
+const Koa = require('koa');
+const Router = require('koa-router');
+const Http = require('http');
+const Querystring = require('querystring')
 
-const app = new Koa()
-const logger = async function(ctx, next){
-  console.log(ctx.method,ctx.host + ctx.url)
-  await next();
+const app = new Koa();
+const router = new Router();
+
+const Service = {
+    search: async (kw = '') => {
+        return new Promise((resolve, reject) => {
+          //http://m.maoyan.com/ajax/search?kw=捉妖记&cityId=10
+            Http.request({
+                hostname: 'm.maoyan.com',
+                path: '/ajax/search?' + Querystring.stringify({
+                    kw,
+                    cityId: 10
+                })
+            }, (res) => {
+
+                res.setEncoding('utf8');
+
+                let data = [];
+
+                res.on('data', (chunk) => {
+                    data.push(chunk)
+                }).on('end', () => {
+                    resolve(data.join(''));
+                });
+
+            }).end();
+        });
+    }
 }
-app.use(logger)
-app.use(bodyparser())
-app.use(async (ctx, next)=>{
-  console.log(ctx.method)
-  if(ctx.url=== '/' && ctx.method === "GET"){
-    ctx.type ='html';
-    let html = `
-    <h1>登录<\h1>
-    <form method = "POST" action= "/">
-    <p>用户名</p>
-    <input name="userName" /><br/>
-    <p>密码</p>
-    <input name="password" type="password" /><br/>
-    <button type="submit">submit</button>
-    </form>
-    `
-    ctx.body=html;
-  }else if(ctx.url === '/' && ctx.method === 'POST'){
-    let postData = ctx.request.body;
-    ctx.body = postData;
-  }
-})
-app.listen(3000)
 
+const Render = (data = {}, kw = '') => {
+
+    let arr = [
+        '<style>',
+        '*{margin:0;padding:0;font-size: 12px;}',
+        'body{padding:10px}',
+        'button{padding: 0 10px;line-height: 20px;margin-left: 10px;}',
+        'input{line-height: 20px; width: 200px;padding: 0 5px;}',
+        'h3{font-size: 14px;}',
+        'i{font-size: 14px;font-style: inherit;color: #f80;}',
+        'p{color:#666;}',
+        '.item{padding:10px 0 10px 0;border-bottom:1px solid #d2d2d2;display:flex;}',
+        '.info{margin-left:10px}',
+        '</style>',
+        '<form><input name="kw" value="' + kw + '"/><button>搜索</button></form>'
+    ];
+
+    data.movies && data.movies.list.forEach(item => {
+        arr.push('<div class="item">')
+        arr.push('<img src="' + item.img.replace('w.h', '64.90') + '"/>')
+        arr.push('<div class="info">')
+        arr.push('<h3>' + item.nm.replace(new RegExp(kw, 'g'), '<i>' + kw + '</i>') + '（' + item.enm + '）' + item.ver + '</h3>')
+        arr.push('<p>' + item.cat + '</p>')
+        arr.push('<p>' + item.star + '</p>')
+        arr.push('<p>' + item.pubDesc + '</p>')
+        arr.push('<p>' + item.sc + '分</p>')
+        arr.push('</div>')
+        arr.push('</div>')
+    });
+
+    return arr.join('')
+}
+
+router.get('/', async (ctx, next) => {
+    let { kw } = ctx.query;
+    let data = await Service.search(kw);
+    ctx.body = Render(JSON.parse(data), kw);
+});
+
+app.use(router.routes()).listen(8080, () => {
+    console.log('Server is running at http://localhost:8080')
+})
