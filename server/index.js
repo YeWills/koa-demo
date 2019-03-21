79a6385ad8da6cc1d0a6f13d79c315522f703182
HZ-1 <601661706@qq.com>
2019-03-22 01:11:02 +0800

    使用koa-multer，做一个文件上传的功能，并且重命名文件，非常好的功能

--- server/index.js
+++ server/index.js
@@ -1,32 +1,49 @@
-const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')
+const Koa = require('koa');
+const fs = require('fs');
+const path = require('path');
+const Router = require('koa-router');
+const multer = require('koa-multer');
+const app = new Koa();
+const router = new Router();
+const upload = multer({
+    dest: 'uploads/' // 指定上传文件的存储目录
+});
+const types = upload.single('avatar');
+router.get('/upload', async (ctx, next) => {
+    ctx.response.body =
+        `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Document</title>
+        </head>
+        <body>
+            <form method='post' action='/profile' enctype='multipart/form-data'>
+            选择图片：<input name="avatar" id='upfile' type='file'/>
+            <input type='submit' value='提交'/>
+        </form>
+        </body>
+    </html>`;
+});
+router.post('/profile', types, async function cb(ctx, next){
+    const {
+        originalname,
+        path: out_path,
+        mimetype
+    } = ctx.req.file;
+    let newName = out_path + path.parse(originalname).ext;
+    // renameSync rename重命名文件名
+    let err = fs.renameSync(out_path, newName);
+    let result;
+    if (err) {
+        result = JSON.stringify(err);
+    } else {
+        result = '<h1>upload success</h1>';
+    }
+    ctx.response.body = result;
+});
 
-const app = new Koa()
-const logger = async function(ctx, next){
-  console.log(ctx.method,ctx.host + ctx.url)
-  await next();
-}
-app.use(logger)
-app.use(bodyparser())
-app.use(async (ctx, next)=>{
-  console.log(ctx.method)
-  if(ctx.url=== '/' && ctx.method === "GET"){
-    ctx.type ='html';
-    let html = `
-    <h1>登录<\h1>
-    <form method = "POST" action= "/">
-    <p>用户名</p>
-    <input name="userName" /><br/>
-    <p>密码</p>
-    <input name="password" type="password" /><br/>
-    <button type="submit">submit</button>
-    </form>
-    `
-    ctx.body=html;
-  }else if(ctx.url === '/' && ctx.method === 'POST'){
-    let postData = ctx.request.body;
-    ctx.body = postData;
-  }
-})
-app.listen(3000)
-
+app.use(router.routes());
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000');
+});
