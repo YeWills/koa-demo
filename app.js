7023e8178a8eb0b847757270e8b9bfdfeb29780e
HZ-1 <601661706@qq.com>
2019-03-23 00:16:56 +0800

    增加500报错的 报错捕获，不过目前没找到如何可以500，看看效果，先把代码提交上来 8.1.3

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ router(app);
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000');
 });
+app.on("error", (err, ctx) => {
+  if (ctx && !ctx.headerSent && ctx.status < 500) {
+    ctx.status = 500;
+  };
+  if (ctx && ctx.log && ctx.log.error) {
+    if (!ctx.state.logged) {
+      ctx.log.error(err.stack);
+    };
+  };
+});
