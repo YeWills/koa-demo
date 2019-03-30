ceef72df153cf028342b2645b7d7dfdf5974bef7
HZ-1 <601661706@qq.com>
2019-03-30 23:32:44 +0800

    返回一个 文件的接口

--- server/index.js
+++ server/index.js
@@ -2,20 +2,42 @@ const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const router = require('koa-router')()
 var cors = require('koa2-cors');
+const fs = require('fs');
+const path = require('path');
+const extname = path.extname;
 const app = new Koa()
- router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>index page</h1>`
-})
-router.get('/home', async (ctx, next) => {
-    ctx.response.body = '<h1>HOME page</h1>'
-})
-router.get('/404', async (ctx, next) => {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-})
 app.use(cors()) // 解决跨域
+
+app.use(async (ctx,next)=>{
+   if(ctx.url === '/getFile' && ctx.method === "GET"){
+     console.log(666)
+    const fpath = path.join(__dirname, './files/test.xlsx');
+    const fstat = await stat(fpath);
+    if (fstat.isFile()) {
+      ctx.type = extname(fpath);
+      ctx.body = fs.createReadStream(fpath);
+    }
+   }else if(ctx.url === '/' && ctx.method === 'POST'){
+    let postData = ctx.request.body;
+    ctx.body = postData;
+  }else{
+    ctx.body='welcome to home page!!';
+  }
+})
+
 app.use(bodyparser())// 解析post参数
-app.use(router.routes())// 调用路由中间件
-app.use(router.allowedMethods())// 对异常状态码处理
 app.listen(3000, ()=>{
   console.log('server is running at http://localhost:3000')
 })
+
+function stat(file) {
+  return new Promise(function(resolve, reject) {
+    fs.stat(file, function(err, stat) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stat);
+      }
+    });
+  });
+}
